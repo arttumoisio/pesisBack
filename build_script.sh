5bc5745cacb0640f2136e3abd68446a8c7c80b5e
Arttu Moisio <arttu.moisio@tuni.fi>
2020-04-24 14:00:45 +0300

    deleted redundant files

--- build_script.sh
+++
@@ -1,9 +0,0 @@
-cd C:/Users/Juulia/arttu/pesis/pesisBack
-docker build . -t back
-heroku container:push web --app pesisback
-heroku container:release web --app pesisback
-
-cd C:/Users/Juulia/arttu/pesis/pesisFront
-docker build . -f ./Dockerfile-build/ -t front
-heroku container:push web --app pesisstats
-heroku container:release web --app pesisstats
